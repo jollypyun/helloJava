2448a05b1e7b2892bb5333a54d8e0834b5231048
geunhyung <lucas.stave@gmail.com>
2023-01-06 09:42:38 +0900

    practicingJava create

---
+++ src/test/java/com/example/practicingjava/PracticingJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practicingjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticingJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
